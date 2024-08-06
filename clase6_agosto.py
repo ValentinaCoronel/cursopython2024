698852af7a4cf1cb2fedfc7464ea44feb0cbfb43
Valentina <valeencoroneel@gmail.com>
2024-08-06 20:35:38 -0300

    subida clase6-8

---
+++ clase6_agosto.py
@@ -0,0 +1,90 @@
+#VGREGHREGRe
+
+
+
+
+
+
+
+
+
+""" def saludar():
+    print("hola, los estoy saludando")
+#para llamarla escribiendo el nombre de la fnuncion 
+saludar()
+def test():
+    variable_test = 10
+    print(variable_test)
+    test() """
+    
+
+""" def test3():
+    return[1,2,3,4,5]
+
+print(test3())
+print(test3()[1])
+print(test3()[1:4])
+
+lista = test3()
+print(lista[-1])
+ 
+def test4():
+    return print("Hola", 30, [1,2,3,4,5,6,7,8])
+test4() """
+
+
+""" def suma(numero1, numero2):
+    return numero1 + numero2
+
+r = suma (7,9)
+print(r) """
+
+""" def duplicar_valor(numero):
+    numero *=2
+
+numero = 10
+duplicar_valor(numero)
+print(numero) """
+
+""" def duplicar_valor(numero):
+    for i,n in enumerate(numero):
+        numero[i] *=2
+        
+lista2 = [10, 50, 100]
+duplicar_valor(lista2)
+
+print(lista2) """
+
+""" def duplicar_valor1(numero):
+    numero*=2
+
+numero = 10
+duplicar_valor1(numero)
+print(numero)
+ """
+
+
+""" def cuenta(numero):
+    numero -=1
+    if numero>0:
+        print(numero)
+        cuenta(numero)
+    else:
+        print("termino")
+cuenta(10)
+
+def calculo_factorial(numero):
+    print("valor inicial", numero)
+    if numero > 1:
+        numero = numero * calculo_factorial(numero -1)
+    print("valor final", numero)
+    return numero
+print(calculo_factorial(5))  """
+   
+print(int(2.5))
+print(int("30"))
+
+print(float("2.5"))
+print(float(25))
+
+print(round(2.6))
